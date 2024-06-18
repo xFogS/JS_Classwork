4b6e1b824bfb65e2dc19a3f45bec32a12daf1320
xFogS <romanprotsenko4@gmail.com>
2024-06-18 21:36:22 +0300

    Upd....

--- 10-06-2024/sTimeout-sInterval.js
+++ 10-06-2024/sTimeout-sInterval.js
@@ -34,13 +34,11 @@ function getPow(num)
 callGetElementByIdAndFunc("btnNodNum", getNod, "Nod");
 function getNod(x, y)
 {
-    console.log(x + ' ' + y)
     if (x == null || y == null) return -1;
     let r = x % y;
     if (r == 0)
-        return y;
+        return y; // we find Nod
     x = y; y = r;
-    console.log(x + ' ' + y)
     return getNod(x, y)
 }
 //maxDigit
@@ -62,45 +60,39 @@ function getSimpleNum(num, divisor = 2)
     if (num < 2) return false
     if (divisor == num) return true
     else if (num % divisor == 0) return false
-    return getSimpleNum(num, divisor + 1)
+
+    return getSimpleNum(num, ++divisor)
 }
 //all multiply
 callGetElementByIdAndFunc("btnAllMultiplyNum", getAllMultiply, "AllMultiply");
 function getAllMultiply(num, startMultiply = 2)
 {
     if (getSimpleNum(num) === true)
-        return document.getElementById("alerts").innerText = "it's simple num"
+        return "it's a simple num"
 
     let i = startMultiply;
     if (i >= num)
-        return document.getElementById("alerts").innerText = "Error/Change num"
+        return "Error/Change num"
 
     for (let j = 1; j <= i; ++j)
     {
         if ((i * j) == num)
-            return
-                document.getElementById("alerts").innerText = `${i} * ${j} = ${num}`
+            return `${i} * ${j} = ${num}`
         for (let k = 1; k <= j; ++k)
         {
             if ((i * j * k) == num)
-                return document.getElementById("alerts").innerText = `${i} * ${j} * ${k} = ${num}`
+                return `${i} * ${j} * ${k} = ${num}`
         }
     }
     return getAllMultiply(num, ++i)
 }
 
 callGetElementByIdAndFunc("btnFindFibonacciNum", getFibonacci, "Fibonacci")
-function getFibonacci(num)
+function getFibonacci(num, a1 = 0, a2 = 1, a3 = 1)
 {
-    let a1 = 0,
-        a2 = 1,
-        a3 = a1 + a2;
-    while (a3 <= num)
-    {
-        if (a3 == num)
-            return document.getElementById("alerts").innerText = `${a1} + ${a2} = ${a3}`;
-        ++a2;
-        a1 = a2; a3 = a1 + a2;
-    }
-    return -1;
+    if (a3 > num) return `${num}\nnot fibonacci`
+
+    if (a3 == num)
+        return `${a1} + ${a2} = ${a3}\nit fibonacci`;
+    return getFibonacci(num, a1 = a2, a2 = a3, a3 = a1 + a2)
 }
