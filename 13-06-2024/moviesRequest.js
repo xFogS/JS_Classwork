4876ad592e588d0e746ddef34fd4c99ba62a816b
FogS <romanprotsenko4@gmail.com>
2024-06-26 22:07:01 +0300

    Upd

---
+++ 13-06-2024/moviesRequest.js
@@ -0,0 +1,43 @@
+/*document.getElementById("listOfMovie").style.visibility = "hidden";
+    document.getElementById("navigatorOfMovie").style.visibility = "hidden"
+*/
+//AJAX
+let imgPoster, category, titleMovie, year;
+let collectionOfMovies = []
+document.getElementById("btnSendRequest").onclick = function ()
+{
+    switch (document.getElementById("inputGroupSelect01").selectedIndex)
+    {
+        case 0: {category = 'Movie'; break;}
+        case 1: {category = 'Series'; break;}
+        case 2: {category = 'Episode'; break;}
+    }
+
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=b307a83f',
+        {
+            method: 'POST',
+            body: JSON.stringify(
+                {
+                    "Title": document.getElementById("inpTitleOfMovie").innerText,
+                    "Type": category
+                }
+            )
+        })
+        .then(res => {
+            if (res.status === 200)
+                return res.json()
+        })
+        .then(response =>
+        {
+            collectionOfMovies = response;
+            getInfoAboutSearch();
+        })
+        .catch(err => console.log(err.message))
+}
+function getInfoAboutSearch()
+{
+    collectionOfMovies.forEach(response =>
+    {
+        console.log(response)
+    })
+}
