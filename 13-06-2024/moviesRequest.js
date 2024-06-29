0cd5b4ce864c7cbc20ab555dc13899595974292c
FogS <romanprotsenko4@gmail.com>
2024-06-29 13:28:05 +0300

    Upd

--- 13-06-2024/moviesRequest.js
+++ 13-06-2024/moviesRequest.js
@@ -2,33 +2,40 @@
     document.getElementById("navigatorOfMovie").style.visibility = "hidden"
 */
 //AJAX
-let imgPoster, category, titleOfMovie, year;
 let collectionOfMovies = []
-document.getElementById("btnSendRequest").onclick = () =>
-{
+document.getElementById("btnSendRequest").onclick = () => {
     indxOfSelectToType = document.getElementById("selectOfType").selectedIndex;
-    category = document.getElementById("selectOfType")[indxOfSelectToType].text;
+    let category = document.getElementById("selectOfType")[indxOfSelectToType].text;
 
     const url = 'http://www.omdbapi.com/?'
     const apiKey = '&apikey=b307a83f'
-    titleOfMovie = document.getElementById("inpTitleOfMovie").value
-    fetch(url + 't=' + titleOfMovie + '&type=' + category.toString().toLowerCase() + apiKey)
+    /*let countOfPage = 1;
+        while (countOfPage <= 10) {*/
+    //let dt = new Date().getFullYear() - (new Date().getFullYear() % parseInt((Math.random() + 1) * 25))
+    let titleOfMovie = document.getElementById("inpTitleOfMovie").value
+    fetch(url + 's=' + titleOfMovie + '&type=' + category.toString().toLowerCase() + apiKey)
         .then(request => {
-            if (request.status === 200)
-                return request.json()
+            if (request.status !== 200) return
+            return request.json()
         })
-        .then(response =>
-        {
-            collectionOfMovies = response
+        .then(response => {
+            collectionOfMovies = response.Search
             getInfoAboutSearch()
         })
+        .catch(err => err.message)
 }
-function getInfoAboutSearch()
-{
+function getInfoAboutSearch() {
+
+    //document.getElementById("listOfMovie").clear()
+    //<img src="" className="card-img-top" alt="..." style="display: inline-flex"/>
+    //console.log("im here")
     console.log(collectionOfMovies)
-    collectionOfMovies.forEach(response =>
+    collectionOfMovies.forEach(res =>
     {
-        imgPoster = response.Poster
-        year = response.Year
+        let imgPoster = document.createElement("img")
+        imgPoster.src = res.Poster
+        imgPoster.className = 'card-img-top'
+        imgPoster.style = "inline-flex"
+        document.getElementById("listOfMovie").appendChild(imgPoster)
     })
 }
