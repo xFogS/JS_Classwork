b4ceb1d14140e9cccabc24ef832dcfb345732b08
FogS <romanprotsenko4@gmail.com>
2024-06-30 19:38:01 +0300

    Upd

--- 17-06-2024/weather.js
+++ 17-06-2024/weather.js
@@ -8,17 +8,18 @@ function buildWeather()
     /*
         http://api.weatherapi.com/v1/current.json?key=0fdff4eec106484ab64130539243006&q=Odesa&aqi=yes
     */
-    fetch(url+apiKey+'&q=Odesa')
+    fetch(url+apiKey+'&q=Kyiv&days=5')
         .then(request => { if (request.status === 200) return request.json()})
         .then(response =>
         {
             arrWeather = response;
             console.log(arrWeather)
+            updateMainInformation()
         })
         .catch(err => console.log(err.message))
 
 }
-function updateInformation()
+function updateMainInformation()
 {
     if (arrWeather === null) return -1;
 
@@ -27,15 +28,18 @@ function updateInformation()
     let condition = document.getElementById("currentWeatherCondition")
     let place = document.getElementById("currentPlace")
     let dateNow = document.getElementById("dtNow")
-    arrWeather.forEach(res =>
-    {
-        image.src = res.current.condition.icon
-        temperature.innerText = res.current.temp_c + '&deg;'
-        condition.innerText = res.current.condition.text
-        place.innerText = res.location.name + ",  " + res.location.region
-        dateNow.innerText = res.location.localtime
-    })
+    let wind = document.getElementById("cWind")
+    let precip = document.getElementById("cPrecip")
+    let pressure = document.getElementById("cPressure")
+
+    image.src = arrWeather.current.condition.icon
+    temperature.innerText = arrWeather.current.temp_c + '℃'
+    wind.innerText = "Wind " + arrWeather.current.wind_kph
+    precip.innerText = "Precip " + arrWeather.current.precip_mm
+    pressure.innerText = "Pressure " + arrWeather.current.pressure_mb
+    condition.innerText = arrWeather.current.condition.text
+    place.innerText = arrWeather.location.country + ",  " + arrWeather.location.name
+    dateNow.innerText = arrWeather.location.localtime
 }
 
 buildWeather()
-updateInformation()
